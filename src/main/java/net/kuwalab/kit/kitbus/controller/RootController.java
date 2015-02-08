104bbed2ccfa187c928002c84824a2ad4ecb8ef9
kuwalab <arata.shinichi@gmail.com>
2015-02-08 19:05:19 +0900

    テスト

--- src/main/java/net/kuwalab/kit/kitbus/controller/RootController.java
+++ src/main/java/net/kuwalab/kit/kitbus/controller/RootController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class RootController {
 	@RequestMapping("/")
 	public String home() {
-		return "Hello world!";
+		return "Hello world";
 	}
 }
