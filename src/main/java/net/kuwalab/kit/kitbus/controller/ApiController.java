5caa0bdfb17f87838cc43a068d06d46d1bf44e5a
kuwalab <arata.shinichi@gmail.com>
2015-02-11 18:43:03 +0900

    ApiController#serivcetableのレスポンスをJSONにする

--- src/main/java/net/kuwalab/kit/kitbus/controller/ApiController.java
+++ src/main/java/net/kuwalab/kit/kitbus/controller/ApiController.java
@@ -1,22 +1,41 @@
 package net.kuwalab.kit.kitbus.controller;
 
+import java.io.IOException;
 import java.util.Optional;
 
 import net.kuwalab.kit.kitbus.util.HttpUtil;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RestController
 @RequestMapping("/api")
 public class ApiController {
-	@RequestMapping("/servicetable")
+	@RequestMapping(value = "/servicetable", method = RequestMethod.GET, produces = "application/json")
 	public String servicetable() {
 		Optional<String> stcsv = HttpUtil.getText(
 				"http://www.kanazawa-it.ac.jp/shuttlebus/servicetable.csv",
 				"Windows-31J");
 		ServiceTable serviceTable = new ServiceTable(stcsv);
 
-		return "Hello world " + serviceTable.toString();
+		ObjectMapper objectMapper = new ObjectMapper();
+		Optional<String> jsonString = null;
+
+		try {
+			jsonString = Optional.of(objectMapper
+					.writeValueAsString(serviceTable));
+		} catch (JsonGenerationException e) {
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return jsonString.orElse("{}");
 	}
 }
